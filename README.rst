74f12e54fb2e1dd8cb9aec401486fe88c1bbb003
Evgeniy Getmanskiy <getmansky@steelkiwi.com>
2016-07-15 18:45:43 +0300

    initial package configuration

---
+++ README.rst
@@ -0,0 +1,44 @@
+===================
+djangocms-rest-api
+===================
+
+An application to use CMS content and features via REST API.
+
+djangocms-rest-api uses Django REST framework to serve django CMS data through a REST API
+
+Installation
+------------
+
+* pip install djangocms-rest-api
+* Edit ``INSTALLED_APPS``::
+
+    INSTALLED_APPS = [
+        ...
+        'rest_framework',
+        'djangocms_rest_api',
+        ...
+    ]
+
+* Edit ``urls.py``::
+
+    urlpatterns = [
+        ...
+        url(r'^api/', include('djangocms_rest_api.urls')),
+        ...
+    ]
+
+* That's all!
+
+
+Features
+--------
+
+
+Credits
+-------
+
+
+License
+-------
+
+MIT
---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def get_version(*file_paths):
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+version = get_version('djangocms_rest_api', '__init__.py')
+
+setup(
+    name='djangocms-rest-api',
+    version=version,
+    packages=['djangocms_rest_api', ],
+    description='API for django cms.',
+    long_description=open('README.rst').read(),
+    author='SteelKiwi Development, Divio',
+    author_email='getmansky@steelkiwi.com',
+    include_package_data=True,
+    install_requires=[
+        'djangorestframework',
+    ],
+    license='MIT',
+    url='https://github.com/divio/djangocms-rest-api',
+    keywords='djangocms-rest-api',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ])
