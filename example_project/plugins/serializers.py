31c7f8990e7ed8209cf599ecf9fe361fbb93c997
Evgeniy Getmanskiy <getmansky@steelkiwi.com>
2016-08-26 09:53:02 +0300

    * custom serializer in example project

---
+++ example_project/plugins/serializers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from rest_framework import serializers
+
+from plugins.models import Slider
+
+
+class SliderWithInlinesPluginSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for slider plugin
+    """
+
+    test = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Slider
+        depth = 1
+        fields = ['id', 'name', 'slides', 'test']
+
+    def get_test(self, obj):
+        return 'test'
