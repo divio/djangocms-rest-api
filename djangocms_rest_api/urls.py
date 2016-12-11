c1ec32f04072971a2888683177c744b81255a5af
Jon Robison <narfman0@gmail.com>
2016-12-11 15:28:00 -0500

    Revert "*remove menu functionality"
    This reverts commit 88495e398150c49b4b6401be31d0647fe5943fbb.

--- djangocms_rest_api/urls.py
+++ djangocms_rest_api/urls.py
@@ -3,9 +3,10 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from rest_framework.routers import DefaultRouter
 
-from djangocms_rest_api.views import PageViewSet, PlaceHolderViewSet, PluginViewSet
+from djangocms_rest_api.views import PageViewSet, PlaceHolderViewSet, PluginViewSet, MenuViewSet
 
 router = DefaultRouter()
+router.register(r'menus', MenuViewSet, 'menu')
 router.register(r'pages', PageViewSet, 'page')
 router.register(r'placeholders', PlaceHolderViewSet, 'placeholder')
 router.register(r'plugins', PluginViewSet, 'plugin')
